db5a3e51ce132e23fa21b671f4e4063717219df8
mtoranzo <marianoeto@hotmail.com>
2023-08-19 16:43:13 -0300

    agrego categoria

--- src/controllers/productManager.js
+++ src/controllers/productManager.js
@@ -95,9 +95,10 @@ export class ProductManager {
     }
 }
 export class Product {
-    constructor(title, description, price, status, thumbnail, code, stock) {
+    constructor(title, description, category, price, status, thumbnail, code, stock) {
         this.title = title
         this.description = description
+        this.category = category
         this.price = price
         this.status = status
         this.thumbnail = thumbnail
@@ -109,37 +110,37 @@ export class Product {
    async cargar () {
         await productManager.addProduct(product1)
         timer:500
-        const product2 = new Product("Yogurt", "Natural", 400, true, "Sin imagen", "lac101", 20)
+        const product2 = new Product("Yogurt", "Natural", "lacteos", 400, true, "Sin imagen", "lac101", 20)
         await productManager.addProduct(product2)
         timer:500
-        const product3 = new Product("Queso", "Cremoso", 2000, true, "Sin imagen", "lac102", 20)
+        const product3 = new Product("Queso", "Cremoso", "lacteos", 2000, true, "Sin imagen", "lac102", 20)
         await productManager.addProduct(product3)
         timer:500
-        const product4 = new Product("Manteca", "Natural", 400, true, "Sin imagen", "lac103", 20)
+        const product4 = new Product("Manteca", "Natural", "lacteos",  400, true, "Sin imagen", "lac103", 20)
         await productManager.addProduct(product4)
         timer:500
-        const product5 = new Product("Margarina", "Untable", 300, true, "Sin imagen", "lac104", 20)
+        const product5 = new Product("Margarina", "Untable", "lacteos", 300, true, "Sin imagen", "lac104", 20)
         await productManager.addProduct(product5)
         timer:500
-        const product6 = new Product("Costilla", "Novillito", 2000, true, "Sin imagen", "Asa100", 20)
+        const product6 = new Product("Costilla", "Novillito", "carnes", 2000, true, "Sin imagen", "Asa100", 20)
         await productManager.addProduct(product6)
         timer:500
-        const product7 = new Product("Vacio", "Novillito", 2500, true, "Sin imagen", "Asa101", 20)
+        const product7 = new Product("Vacio", "Novillito", "carnes", 2500, true, "Sin imagen", "Asa101", 20)
         await productManager.addProduct(product7)
         timer:500
-        const product8 = new Product("Tapa de Asado", "Novillito", 2000, true, "Sin imagen", "Asa102", 20)
+        const product8 = new Product("Tapa de Asado", "Novillito", "carnes", 2000, true, "Sin imagen", "Asa102", 20)
         await productManager.addProduct(product8)
         timer:500
-        const product9 = new Product("Falda", "Novillito", 1500, true, "Sin imagen", "Asa103", 20)
+        const product9 = new Product("Falda", "Novillito", "carnes", 1500, true, "Sin imagen", "Asa103", 20)
         await productManager.addProduct(product9)
         timer:500
-        const product10 = new Product("Entrecot", "Novillito", 2000, true, "Sin imagen", "Asa104", 20)
+        const product10 = new Product("Entrecot", "Novillito", "carnes", 2000, true, "Sin imagen", "Asa104", 20)
         await productManager.addProduct(product10)
         }
     } 
 
 const productManager = new ProductManager('./src/products.json')
-const product1 = new Product("Leche", "Entera", 1000, true, "Sin imagen", "lac100", 20)
+const product1 = new Product("Leche", "Entera", "lacteos", 1000, true, "Sin imagen", "lac100", 20)
 product1.cargar()
 
 
