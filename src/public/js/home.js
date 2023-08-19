131a9d624a74afd1bcaf4e666fb819b527d565df
mtoranzo <marianoeto@hotmail.com>
2023-08-19 19:06:04 -0300

    mejoras vista realtime

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -6,6 +6,14 @@ const arrayProductos = document.getElementById('productos')
 socket.on('lista', listado => {
     arrayProductos.innerHTML = "" //Limpio el html
     listado.forEach(producto => {
-        arrayProductos.innerHTML += `<p>Nombre: ${producto.title}\n , Descripcion: ${producto.description} , Categoria: ${producto.category}</p>`
+        arrayProductos.innerHTML += 
+        `<p>Nombre: ${producto.title} 
+        Descripcion: ${producto.description}  
+        Categoria: ${producto.category},
+        Precio: ${producto.price} , 
+        Stock: ${producto.stock},
+        Codigo: ${producto.code} , 
+        Status: ${producto.status}
+        </p>`
     })
 })
