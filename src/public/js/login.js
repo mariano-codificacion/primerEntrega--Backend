c8b0d4f484877a208e03971315382cd98db09806
mtoranzo <marianoeto@hotmail.com>
2023-09-21 14:15:59 -0300

    cambio de rutas action

--- src/public/js/login.js
+++ src/public/js/login.js
@@ -4,7 +4,7 @@ signIn.addEventListener("submit", (e)=>{
     const formData = new FormData(e.target);
     const login = Object.fromEntries(formData);
     console.log("Datos enviados", login);
-    fetch('/login',{
+    fetch('api/sessions/login',{
         method:"POST",
         body:formData
     })
