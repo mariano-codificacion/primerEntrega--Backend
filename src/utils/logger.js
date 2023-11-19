6f416f43bf32551a69f7783697ebdb14feffc98f
mtoranzo <marianoeto@hotmail.com>
2023-11-18 21:38:22 -0300

    mejoras logger

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,38 +1,39 @@
 import winston from "winston";
 
-//export const logger = winston.createLogger({
-const customLevels = {
+const customOptionsLevels = {
     levels: {
-        fatal:0,
+        fatal: 0,
         error: 1,
-        warning: 2,
-        info: 3
+        warn: 2,
+        info: 3,
+        http: 4,
+        debug: 5,
     },
     colors: {
-    fatal: 'red',
-    error: 'mayenta',
-    warning: 'yelow',
-    info: 'blue'
-},
-}
-winston.addColors(customLevels.colors)
+        fatal: 'red',
+        error: 'magenta',
+        warn: 'yellow',
+        info: 'blue',
+        http: 'green',
+        debug: 'white',
+    }
+};
+
 const logger = winston.createLogger({
-
-    levels: customLevels.levels,
-
+    levels: customOptionsLevels.levels,
+  
     transports: [
-    new winston.transports.Console ({
-        level: 'info', 
-        format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.simple()
-        )
-    }),
-    new winston.transports.File({
-        filename: './src/logs/errors.log', 
-        level: 'error',
-    })
-],
+        new winston.transports.Console({
+         
+            level: 'info',
+
+            format: winston.format.combine(
+                winston.format.colorize({colors: customOptionsLevels.colors} ),
+                winston.format.simple()
+            )
+        }),
+        new winston.transports.File({filename: './logs/errors.log',  level: 'warn' })
+    ]
 })
 
 export default logger
